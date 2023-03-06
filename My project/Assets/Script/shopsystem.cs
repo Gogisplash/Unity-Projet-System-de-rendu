40522d6221a4730b9551ebaa26827e8e9a5cfc13
unknown <fquach@gamingcampus.local>
2023-03-06 14:15:24 +0100

    floteur

---
+++ My project/Assets/Script/shopsystem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using UnityEngine;
+
+public class shopsystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //money = GetComponent<InventoryManager>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
